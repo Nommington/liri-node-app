4928a37b190999f18f163c4b59f0262d1f993757
Nommington <rymckenz@gmail.com>
2018-05-19 01:24:24 -0400

    Project complete. Included inquirer functionality.

--- liri.js
+++ liri.js
@@ -3,65 +3,141 @@ var keys = require("./keys.js");
 var request = require("request");
 var spotify = require("node-spotify-api");
 var twitter = require('twitter');
+var inquirer = require("inquirer");
+var fs = require("fs");
+var empty = false;
+var input = process.argv;
+var boringOper = input[2];
+//construct boringParam
+var boringParam = "";
+for (i = 3; i < input.length; i++) {
+    boringParam = boringParam + " " + input[i];
+}
 
-var operator = process.argv[2];
-var param = process.argv[3];
-var vanilla = true;
 var sptfy = new spotify(keys.spotify);
 var client = new twitter(keys.twitter);
 
-//console.log(spotify);
-//console.log(client);
 
-switch (operator){
-    case "my-tweets":
-        //console.log("tweet tweet");
-        var params = {screen_name: 'hringtnid'};
-        client.get('statuses/user_timeline', params, function(error, tweets, response) {
-        if (!error) {
-            for (i=0; i<20; i++){
-            console.log(tweets[i].text);
-      }
-  }
-}).Then(boring());
-    break;
-    case "spotify-this-song":
-        //console.log("music");
-        sptfy.search({ type: 'track', query: param }, function(err, data) {
-            if (err) {
-              return console.log('Error occurred: ' + err);
-            }           
-          //console.log(JSON.stringify(data.tracks.items[0], null, 2));
-          console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name));
-          console.log(JSON.stringify(data.tracks.items[0].album.name));
-          console.log(JSON.stringify(data.tracks.items[0].preview_url));
-          }).Then(boring());
-    break;
-    case "movie-this":
-        //console.log("motion picture movies!");
-        request("http://www.omdbapi.com/?apikey=trilogy&t="+param, function (error, response, body){
-            //console.log("error:", error);
-            var movie = JSON.parse(body);
-            console.log(movie.Title);
-            console.log(movie.Year);
-            for (i=0; i<movie.Ratings.length; i++){
-                console.log(movie.Ratings[i].Source);
-                console.log(movie.Ratings[i].Value);
+function boring() {
+    console.log("");
+    console.log("You did it the boring way.");
+    console.log("Try just typing 'node liri'.");
+    console.log("");
+}
+
+function interesting() {
+    console.log("Welcome to LIRI!");
+    console.log("");
+    console.log("LIRI is a Language Interpretation and Recognition Interface using NodeJS.");
+    console.log("");
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Which of LIRI's functions would you like to utilize?",
+                choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"],
+                name: "oper"
+            },
+            {
+                type: "input",
+                message: "Please type the Twitter handle/movie/song you would like LIRI to search.",
+                name: "param"
             }
-            console.log(movie.Country);
-            console.log(movie.Language);
-            console.log(movie.Plot);
-            console.log(movie.Actors);
-        }).Then(boring());
-    break;
-    case "do-what-it-says":
-        console.log("do that thing");
-    break;
+        ]).then(function(inquirerResponse){
+            var interestingOper = inquirerResponse.oper;
+            var interestingParam = inquirerResponse.param;
+            doTheThing(interestingOper,interestingParam);
+        });
+}
+
+function doTheThing(oper, param) {
+    switch (oper) {
+        case "my-tweets":
+            if (empty) {
+                console.log("Since you didn't specify, please enjoy some stupid old tweets from LIRI's creator's old account.");
+                console.log("He is very sorry.");
+                console.log("");
+                param = "hringtnid";
+            }
+            var handle = { screen_name: param };
+            console.log("Here are the 20 most recent tweets by " + handle + ":");
+            console.log("");
+            client.get('statuses/user_timeline', handle, function (error, tweets, response) {
+                if (!error) {
+                    for (i = 0; i < 20; i++) {
+                        console.log(tweets[i].text);
+                    }
+                }
+            });
+            break;
+        case "spotify-this-song":
+            if (empty) {
+                console.log("Since you didn't specify, per assignment instructions, here is info on a catchy song written by a band who were likely actual Nazis. Look it up.");
+                console.log("LIRI is very sorry.");
+                console.log("");
+                param = "I saw the sign";
+            }
+            sptfy.search({ type: 'track', query: param }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                //console.log(JSON.stringify(data.tracks.items[0], null, 2));
+                console.log("Song Name: " + JSON.stringify(data.tracks.items[0].name));
+                console.log("Artist: " + JSON.stringify(data.tracks.items[0].album.artists[0].name));
+                console.log("Album of Origin: " + JSON.stringify(data.tracks.items[0].album.name));
+                console.log("Preview URL: " + JSON.stringify(data.tracks.items[0].preview_url));
+            });
+            break;
+        case "movie-this":
+            if (empty) {
+                console.log("Since you didn't specify, here is info for a movie about somebody who is nobody. LIRI's creator hasn't seen it.");
+                console.log("He is very sorry.");
+                console.log("");
+                param = "mr nobody";
+            }
+            request("http://www.omdbapi.com/?apikey=trilogy&t=" + param, function (error, response, body) {
+                //console.log("error:", error);
+                var movie = JSON.parse(body);
+                console.log("Movie Title: " + movie.Title);
+                console.log("Year of Release: " + movie.Year);
+                for (i = 0; i < movie.Ratings.length; i++) {
+                    console.log("Rating on " + movie.Ratings[i].Source +": " + movie.Ratings[i].Value);
+                }
+                console.log("Country of Origin: " + movie.Country);
+                console.log("Language: " + movie.Language);
+                console.log("");
+                console.log("Plot Synopsis:");
+                console.log(movie.Plot);
+                console.log("");
+                console.log("Principle Cast:");
+                console.log(movie.Actors);
+            });
+            break;
+        case "do-what-it-says":
+            console.log("You've ceded control to LIRI.");
+            console.log("Your search parameters, if you entered any, are irrelevant.");
+            console.log("LIRI will display information about one of LIRI creator's favorite movies.");
+            console.log("");
+            fs.readFile("random.txt", "utf8", function(error,data){
+                if (!error){
+                    var inputArr = data.split(",");
+                    empty = false;
+                    doTheThing(inputArr[0], inputArr[1]);
+                }
+            });
+            break;
     }
-    function boring(){
-        if (vanilla){
-            console.log("You did it the boring way.");
-            console.log("Try just typing 'node liri'.");
-        }
+}
+
+if (input.length > 2) {
+    boring();
+    if (input.length == 3) {
+        empty = true;
     }
+    doTheThing(boringOper, boringParam);
+}
+else if (input.length == 2){
+    interesting();
+}
+
 
